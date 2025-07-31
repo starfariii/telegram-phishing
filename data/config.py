369833cce35ce6a5bf719b910dfd24919f031245
starfariii <o2has20000@gmail.com>
2025-07-31 15:39:15 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,14 +1,12 @@
 from utils.postgres_db import DB
 import os
 
-
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 KEY_DOMAIN = os.getenv("KEY_DOMAIN")
 
-DP_PORT = 5432
-DB_USER="postgres"
+DB_PORT = 5432       
+DB_USER = "postgres"
 DB_PASS = os.getenv("DB_PASS")
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = "railway"
@@ -17,10 +15,10 @@ DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 db: DB = DB(
     DB_HOST, DB_PORT, DB_USER,
-    DP_PASSWORD, DB_NAME
+    DB_PASS, DB_NAME       
 )
 
-BAD_SYMBOLS = ['+', '(', '-', ')', ' '] # не трогать
+BAD_SYMBOLS = ['+', '(', '-', ')', ' ']  # не трогать
 
 SESSIONS_STORAGE = "sessions"
 os.makedirs(SESSIONS_STORAGE, exist_ok=True)
