d4c3cd7d4aeb3a58ee12e5773031d5365b1ed74e
starfariii <o2has20000@gmail.com>
2025-07-31 15:33:55 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -16,8 +16,8 @@ DB_NAME = "railway"
 DB_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 
 db: DB = DB(
-    db_host, db_port, db_user,
-    dp_password, db_name
+    DB_HOST, DB_PORT, DB_USER,
+    DP_PASSWORD, DB_NAME
 )
 
 BAD_SYMBOLS = ['+', '(', '-', ')', ' '] # не трогать
